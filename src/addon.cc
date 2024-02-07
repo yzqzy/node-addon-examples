30deb6a7570158e1d441fa8d7a1fabbd8473e53f
杨志强 <yangzhiqiang@winwin-inc.com>
2024-02-07 18:28:18 +0800

    perf: update input

--- src/addon.cc
+++ src/addon.cc
@@ -1,45 +1,45 @@
 #include <napi.h>
 
 class ExampleAddon : public Napi::Addon<ExampleAddon> {
-  public:
-    ExampleAddon(Napi::Env env, Napi::Object exports) {
-        // In the constructor we declare the functions the add-on makes
-        // available to JavaScript.
-        DefineAddon(exports, {InstanceMethod("hello", &ExampleAddon::Hello),
-                              InstanceMethod("increment", &ExampleAddon::Increment),
-                              InstanceMethod("decrement", &ExampleAddon::Decrement),
+ public:
+  ExampleAddon(Napi::Env env, Napi::Object exports) {
+    // In the constructor we declare the functions the add-on makes
+    // available to JavaScript.
+    DefineAddon(exports,
+                {InstanceMethod("hello", &ExampleAddon::Hello), InstanceMethod("increment", &ExampleAddon::Increment),
+                 InstanceMethod("decrement", &ExampleAddon::Decrement),
 
-                              // We can also attach plain objects to `exports`, and instance
-                              // methods as properties of those sub-objects.
-                              InstanceValue("subObject",
-                                            DefineProperties(Napi::Object::New(env),
-                                                             {InstanceMethod("increment", &ExampleAddon::Increment),
-                                                              InstanceMethod("decrement", &ExampleAddon::Decrement)}),
-                                            napi_enumerable)});
-    }
+                 // We can also attach plain objects to `exports`, and instance
+                 // methods as properties of those sub-objects.
+                 InstanceValue(
+                     "subObject",
+                     DefineProperties(Napi::Object::New(env), {InstanceMethod("increment", &ExampleAddon::Increment),
+                                                               InstanceMethod("decrement", &ExampleAddon::Decrement)}),
+                     napi_enumerable)});
+  }
 
-  private:
-    Napi::Value Hello(const Napi::CallbackInfo& info) {
-        return Napi::String::New(info.Env(), "hello world");
-    }
+ private:
+  Napi::Value Hello(const Napi::CallbackInfo &info) {
+    return Napi::String::New(info.Env(), "hello world");
+  }
 
-    // This method has access to the data stored in the environment because it
-    // is an instance method of `ExampleAddon` and because it was listed among
-    // the property descriptors passed to `DefineAddon()` in the constructor.
-    Napi::Value Increment(const Napi::CallbackInfo& info) {
-        return Napi::Number::New(info.Env(), ++value);
-    }
+  // This method has access to the data stored in the environment because it
+  // is an instance method of `ExampleAddon` and because it was listed among
+  // the property descriptors passed to `DefineAddon()` in the constructor.
+  Napi::Value Increment(const Napi::CallbackInfo &info) {
+    return Napi::Number::New(info.Env(), ++value);
+  }
 
-    // This method has access to the data stored in the environment because it
-    // is an instance method of `ExampleAddon` and because it was exposed to
-    // JavaScript by calling `DefineProperties()` with the object onto which it
-    // is attached.
-    Napi::Value Decrement(const Napi::CallbackInfo& info) {
-        return Napi::Number::New(info.Env(), --value);
-    }
+  // This method has access to the data stored in the environment because it
+  // is an instance method of `ExampleAddon` and because it was exposed to
+  // JavaScript by calling `DefineProperties()` with the object onto which it
+  // is attached.
+  Napi::Value Decrement(const Napi::CallbackInfo &info) {
+    return Napi::Number::New(info.Env(), --value);
+  }
 
-    // Data stored in these variables is unique to each instance of the add-on.
-    uint32_t value = 42;
+  // Data stored in these variables is unique to each instance of the add-on.
+  uint32_t value = 42;
 };
 
 // The macro announces that instances of the class `ExampleAddon` will be
--- src/hello.cc
+++ src/hello.cc
@@ -1,14 +1,13 @@
 #include <napi.h>
 
-Napi::String Hello(const Napi::CallbackInfo& info) {
-	Napi::Env env = info.Env();
-	return Napi::String::New(env, "hello world");
+Napi::String Hello(const Napi::CallbackInfo &info) {
+  Napi::Env env = info.Env();
+  return Napi::String::New(env, "hello world");
 }
 
 Napi::Object Init(Napi::Env env, Napi::Object exports) {
-	exports.Set(Napi::String::New(env, "hello"),
-	            Napi::Function::New(env, Hello));
-	return exports;
+  exports.Set(Napi::String::New(env, "hello"), Napi::Function::New(env, Hello));
+  return exports;
 }
 
 NODE_API_MODULE(NODE_GYP_MODULE_NAME, Init)
